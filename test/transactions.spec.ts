a3286a1cd8e45145107c733d1317933337c1e7e8
Luiza Rodrigues <luizars@id.uff.br>
2024-01-04 23:31:04 -0300

    test create a transaction and list all transactions

---
+++ test/transactions.spec.ts
@@ -0,0 +1,45 @@
+import {expect, it, beforeAll, afterAll, describe} from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+    
+    afterAll( async () => {
+        await app.close()
+    })
+    
+    it('should be able to create a new transaction', async () => {
+        await request(app.server).post('/transactions').send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+        .expect(201)
+    })
+
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server).post('/transactions').send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+        
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsResponse = await request(app.server)
+        .get('/transactions')
+        .set('Cookie', cookies)
+        .expect(200)
+
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000
+            })
+        ])
+    })
+})
+
